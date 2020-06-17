ec5bb37b86d658747339828c56e5d7c69035333c
ShortJB <749997665@qq.com>
2020-06-17 15:34:44 +0800

    - [太极秀] 熟练动画编辑器

---
+++ README.md
@@ -0,0 +1,9 @@
+# [Creator图文教程——《太极秀》（一）](https://forum.cocos.org/t/creator/37160)
+
+
+
+Creator版本：v2.3.3
+
+场景move:
+
+> 动画测试
---
+++ assets/scripts/logo.js
@@ -0,0 +1,18 @@
+
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        
+    },
+
+    onLoad () {
+
+    },
+
+    load_story_scene: function () {
+        cc.director.loadScene("story");
+    },
+
+});
